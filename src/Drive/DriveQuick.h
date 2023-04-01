77ef97d4dde2a9df89894acb494337213a764180
RyzenFromFire <ryzenfromfire@gmail.com>
2023-03-31 23:10:02 -0400

    made accel not basically zero

--- src/Drive/DriveQuick.h
+++ src/Drive/DriveQuick.h
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 // the minimum power that can be written to the motor, prevents stalling
 #define MOTOR_ZERO_OFFST 0
-#define RB_ACCELERATION_RATE 0.000001f
+#define RB_ACCELERATION_RATE 0.03f
 #include <Drive/Drive.h>
 
 class DriveQuick : public Drive {
