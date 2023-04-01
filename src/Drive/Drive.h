1717b99b0d0f7bb3171b749cdd6a4690d74de582
RyzenFromFire <ryzenfromfire@gmail.com>
2023-04-01 10:53:15 -0400

    qb speed

--- src/Drive/Drive.h
+++ src/Drive/Drive.h
@@ -55,9 +55,15 @@
 
 // MOTOR MAX SPEED DEFINES;
 // this is 1.0, the maximum power possible to the motors.
+#if BOT_TYPE == 4
+#define BIG_BOOST_PCT 0.7  // default: 0.6, this is the typical percentage of power out of the motors' range that is used (to ensure they don't do seven wheelies)
+#define BIG_NORMAL_PCT 0.3 // should be a value less than BIG_NORMAL_PCT, to slow down for precision maneuvering, QB needs this to be 0.3
+#define BIG_SLOW_PCT 0.2
+#else
 #define BIG_BOOST_PCT 0.7  // default: 0.6, this is the typical percentage of power out of the motors' range that is used (to ensure they don't do seven wheelies)
 #define BIG_NORMAL_PCT 0.6 // should be a value less than BIG_NORMAL_PCT, to slow down for precision maneuvering, QB needs this to be 0.3
 #define BIG_SLOW_PCT 0.3   // the value for brake button to slow down the motors at the button press
+#endif
 
 // BSN for Short/Small Motors
 #define SMALL_BOOST_PCT 0.85
