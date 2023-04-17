6a8f41f3b2d3340006ac8b7ece211bb86366cd40
rdavies02 <72978944+rdavies02@users.noreply.github.com>
2023-04-17 14:18:45 -0400

    progress
    not sure if im overcomplicating this yet. Have preliminary read and write .cpp files made up to test tonight

--- src/Utilities/ReadBotInfo.cpp
+++ src/Utilities/ReadBotInfo.cpp
@@ -1,25 +1,40 @@
 #include <Arduino.h>
-#include <EEPROM.h>
+#include <Preferences.h>
+#include <PolarRobotics.h>
 
-int robotTyp = 0;
-int robotAge = 0;
+Preferences preferences;
 
 void setup() {
-
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println("Reading");
-  robotAge = EEPROM.read(0);
-  Serial.println(robotAge);
-  // 0 = old robot
-  // 1 = new robot
-  robotTyp = EEPROM.read(1);
-  Serial.println(robotTyp);
-  // 0 = Lineman
-  // 1 = Recever
-  // 2 = Center
-  // 3 = Quarterback
-  // 4 = Kicker
-  Serial.println(EEPROM.read(2));
+
+  // open the "bot_name_idx" namespace and set it to read/write
+  preferences.begin("bot_name_idx", true); 
+  // read the bot name
+  Serial.print(F("Bot Name: "));
+  Serial.print(preferences.getUChar("bot_name_idx"));
+  Serial.print(F("\r\n"));
+  // close the namespace
+  preferences.end();
+
+  // open the "bot_type" namespace and set it to read/write
+  preferences.begin("bot_type", true); 
+  // read the bot type
+  Serial.print(F("Bot Type: "));
+  Serial.print(preferences.getUChar("bot_type"));
+  Serial.print(F("\r\n"));
+  // close the namespace
+  preferences.end();
+
+  // open the "motor_type" namespace and set it to read/write
+  preferences.begin("motor_type", true); 
+  // read the bot type
+  Serial.print(F("motor Type: "));
+  Serial.print(preferences.getUChar("bot_type"));
+  Serial.print(F("\r\n"));
+  // close the namespace
+  preferences.end();
+
   Serial.println("Done");
 }
 
