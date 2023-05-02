7a53f8223da8e3326a281272c22bc4d6ff31bf5f
rdavies02 <72978944+rdavies02@users.noreply.github.com>
2023-05-01 21:49:22 -0400

    Changed some formatting

--- src/Utilities/WriteBotInfo.cpp
+++ src/Utilities/WriteBotInfo.cpp
@@ -8,16 +8,19 @@
 
 ConfigManager config;
 
+#define NUM_BOT_TYPES 7
+#define NUM_MOTORS 4
+
 void setup() {
   Serial.begin(115200);
-  Serial.println(F("Writing Bot Type"));
+  Serial.println(F("Writing Bot Type\n"));
 
   //! If you want to use a predefined robot from BotTypes.h, declare the index here:
   uint8_t index = 0; // also handles bot name index
   
   //! If you want to set custom bot and motor type, assign index appropriately, then assign these:
-  eBOT_TYPE bot_type = quarterback; 
-  eMOTOR_TYPE motor_type = mecanum;
+  eBOT_TYPE bot_type = lineman; 
+  eMOTOR_TYPE motor_type = big;
 
   //* Write standard bot configuration from BotTypes.botConfigArray
   if (config.setConfig(index))
