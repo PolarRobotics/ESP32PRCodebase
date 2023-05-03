722979516f7982ed7fec32f86100063f8ad9389f
rdavies02 <72978944+rdavies02@users.noreply.github.com>
2023-05-02 22:18:27 -0400

    Fixed Pairing undefined reference issue

--- src/Robot/builtInLED.h
+++ src/Robot/builtInLED.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#ifndef __BUILT_IN_LED_H__
+#define __BUILT_IN_LED_H__
+
 /**
  * @brief Header for Global ESP Built-In LED Handling
  * @authors Max Phillips
@@ -16,3 +19,5 @@
 bool builtInLedOn();
 void toggleBuiltInLED();
 void setBuiltInLED(bool on = true);
+
+#endif // !__BUILT_IN_LED_H__
