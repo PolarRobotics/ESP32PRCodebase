25c651e0d60313d499fb4098e70842c7448685c4
Tw1ZZLER <ilekbrownies@gmail.com>
2023-09-19 02:10:53 -0400

    New PIO environment
    Created new PIO environment, as well as created a script program file to start working on a script for data acquisition.

---
+++ src/Utilities/DataAcquisition.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+// #include <Preferences.h>
+// #include <PolarRobotics.h>
+#include <Utilities/ConfigManager.h>
+
+void setup() {
+    // Insert Script to send data from sensors to Raspberry Pi here!
+}
+
+void loop() {}
