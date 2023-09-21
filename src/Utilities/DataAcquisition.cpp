f37e3c06a3689bbe6e091ba96aee4a5e1d48f32e
Tw1ZZLER <ilekbrownies@gmail.com>
2023-09-20 21:06:57 -0400

    Created ascending number sample script
    This is not the final product of course, just a test script to test getting data onto a Raspberry Pi.

--- src/Utilities/DataAcquisition.cpp
+++ src/Utilities/DataAcquisition.cpp
@@ -3,8 +3,15 @@
 // #include <PolarRobotics.h>
 #include <Utilities/ConfigManager.h>
 
-void setup() {
-    // Insert Script to send data from sensors to Raspberry Pi here!
-}
+int ascendingNumber = 0;
 
-void loop() {}
+void setup() {
+    Serial.begin(115200);
+}
+    // Insert Script to send data from sensors to Raspberry Pi here!
+void loop() {
+    Serial.print("Ascending number = ");
+    Serial.println(ascendingNumber);
+    ascendingNumber++;
+    delay(500);
+}
