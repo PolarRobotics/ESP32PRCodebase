f851e625aeb5038b8bd8bc988d9319cfdbe2183e
RyzenFromFire <ryzenfromfire@gmail.com>
2023-10-13 19:30:36 -0400

    Update version

--- src/PolarRobotics.h
+++ src/PolarRobotics.h
@@ -12,7 +12,7 @@
 #include <Utilities/BotTypes.h>
 #include <Utilities/MotorTypes.h>
 
-#define PR_CODEBASE_VERSION "2.0.0 @ dev/main"
+#define PR_CODEBASE_VERSION "2.0.0 @ production"
 
 // [PIN DECLARATIONS]
 // please follow: 
