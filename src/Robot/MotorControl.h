ff9fce9117bc9b6a9d7176aeab2f190188189e0a
huckleberri6 <j-devore.1@onu.edu>
2023-10-16 20:12:23 -0400

    update for ramp in motorControl

--- src/Robot/MotorControl.h
+++ src/Robot/MotorControl.h
@@ -49,6 +49,10 @@ private:
   uint32_t tempTimeon;
   uint16_t power2Duty(float power);
 
+  // for ramp
+  float currentPower;     
+  float lastRampTime;
+
   // Encoder
   bool has_encoder;
   uint8_t encoderIndex;
@@ -83,6 +87,8 @@ public:
 
   int Percent2RPM(float pct);
   float RPM2Percent(int rpm);
+
+  int ramp(float requestedPower, float accelRate);
 };
 
 // MotorControl* GlobalClassPointer[MAX_NUM_ENCODERS];
