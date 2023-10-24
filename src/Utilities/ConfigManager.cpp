a057cc9e1e419dfa6ba1584bde1985ed1a3fd107
e-burch3 <143116447+e-burch3@users.noreply.github.com>
2023-10-23 20:52:15 -0400

    start fixing cfg

--- src/Utilities/ConfigManager.cpp
+++ src/Utilities/ConfigManager.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+#define CODE_VERSION_PREF_KEY "version"
+#define NO_VERSION_PLACEHOLDER "NO_VERSION"
+
 ConfigManager::ConfigManager() {
   this->config = new bot_config_t();
 }
@@ -48,11 +51,12 @@ void ConfigManager::read() {
  * @brief gets the code version from the configuration.
  * @return String - the version as a string
  */
-String ConfigManager::version() {
-  preferences.begin("version", true);
-  String str = preferences.getString("code_version");
+const char* ConfigManager::version() {
+  bool good = preferences.begin(CODE_VERSION_PREF_KEY, true);
+  if (!good) return NO_VERSION_PLACEHOLDER;
+  String str = preferences.getString(CODE_VERSION_PREF_KEY, NO_VERSION_PLACEHOLDER);
   preferences.end();
-  return str;
+  return str.c_str();
 }
 
 /**
@@ -109,7 +113,7 @@ float ConfigManager::getWheelBase() {
 const char * ConfigManager::toString() {
     string temp = "\nBot info: ";
     temp.append("\ncode version: ");
-    temp.append(version().c_str());
+    temp.append(version());
     temp.append("\nbot array index #: ");
     temp.append(to_string(config->index));
     temp.append("\nbot name: ");
@@ -161,9 +165,9 @@ bool ConfigManager::write(bot_config_t *cfg) {
 
     // store version to preferences
 
-    good = preferences.begin("version", false); 
+    good = preferences.begin(CODE_VERSION_PREF_KEY, false); 
     if (!good) return false;
-    preferences.putString("version", PR_CODEBASE_VERSION);
+    preferences.putString(CODE_VERSION_PREF_KEY, PR_CODEBASE_VERSION);
     preferences.end();
 
     return true;
