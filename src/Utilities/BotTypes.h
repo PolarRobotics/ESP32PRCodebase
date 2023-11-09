48b6bf94152ea3b7339adf5176b98265ade1660d
t-schira <t-schira@onu.edu>
2023-11-08 19:51:50 -0500

    Runningback turn radius
    lowered minimum turning radius on running back "c" .  Rmin was originally 9.00, now set to 4.00.  Approved by Max P. and Landon

--- src/Utilities/BotTypes.h
+++ src/Utilities/BotTypes.h
@@ -73,7 +73,7 @@ constexpr bot_config_t botConfigArray[NUM_BOTS] = {
   { 6,  ">=",       lineman,     big_ampflow,    { 1.0f,      10.00f,  6.00f,  27.00f }},  // 6:  >=
   { 7,  "32.2",     receiver,    small_ampflow,  { 0.5f,      11.50f,  9.00f,  36.00f }},  // 7:  32.2
   { 8,  "9.8",      receiver,    big_ampflow,    { 0.5f,      11.50f,  9.00f,  36.00f }},  // 8:  9.8
-  { 9,  "c",        runningback, falcon,         { 0.5f,      8.00f ,  9.00f,  36.00f }},  // 9:  c
+  { 9,  "c",        runningback, falcon,         { 0.5f,      8.00f ,  4.00f,  36.00f }},  // 9:  c
   { 10, "phi",      center,      small_ampflow,  { 0.6f,      11.50f,  9.00f,  36.00f }},  // 10: Φ
   { 11, "inf",      quarterback, small_ampflow,  { 0.5625f,   11.50f,  9.00f,  36.00f }},  // 11: ∞
   { 12, "theta",    kicker,      small_ampflow,  { 0.34375f,  10.00f,  9.00f,  36.00f }}   // 12: Θ
