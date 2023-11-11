62bc60e126063877165578c6975c08abb2645e9b
RyzenFromFire <ryzenfromfire@gmail.com>
2023-11-11 08:54:41 -0500

    tuning runningback, parameterize turn function
    allow selecting linear turn via drive constructor + default parameter.

--- src/Drive/Drive.h
+++ src/Drive/Drive.h
@@ -108,7 +108,7 @@ class Drive {
 
     Drive();
     Drive(BotType botType, MotorType motorType);
-    Drive(BotType botType, MotorType motorType, drive_param_t driveParams, bool hasEncoders = false);
+    Drive(BotType botType, MotorType motorType, drive_param_t driveParams, bool hasEncoders = false, int turnFunction = 2);
     void setServos(uint8_t lpin, uint8_t rpin);
     void setServos(uint8_t lpin, uint8_t rpin, uint8_t left_enc_a_pin, uint8_t left_enc_b_pin, uint8_t right_enc_a_pin, uint8_t right_enc_b_pin);
     void setMotorType(MotorType motorType);
--- src/Drive/DriveQuick.cpp
+++ src/Drive/DriveQuick.cpp
@@ -15,7 +15,7 @@ Features:
 */
 
 //! Must call base class constructor with appropriate arguments
-DriveQuick::DriveQuick(drive_param_t driveParams) : Drive(BotType::runningback, MotorType::falcon, driveParams) {
+DriveQuick::DriveQuick(drive_param_t driveParams) : Drive(BotType::runningback, MotorType::falcon, driveParams, false, 0) {
   // initialize array
   // for (int i = 0; i < NUM_MOTORS; i++) {
   //   falcon_motor_pwr[i] = 0.0f;
