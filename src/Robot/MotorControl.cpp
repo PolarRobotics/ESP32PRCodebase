ea71af82da9e87f9e2214f47d7cb0007826446b5
Gib-Hub789 <g-brautigam@onu.edu>
2023-11-19 19:30:54 -0500

    not everthing was in the last one but same thing

--- src/Robot/MotorControl.cpp
+++ src/Robot/MotorControl.cpp
@@ -44,6 +44,14 @@
 MotorControl::MotorControl() {  
   requestedRPM = 0;
   lastRampTime = millis();
+
+  CL_enable = false;
+
+  // if (has_encoder) {
+  //   this->encoderIndex = EncoderCount;
+  //   GlobalClassPointer[EncoderCount++] = this;
+  //   init_encoder();
+  // }
 }
 
 
@@ -78,8 +86,198 @@ void MotorControl::write(float pct) {
   Motor.write(pct);
 }
 
-int MotorControl::Percent2RPM(float pct)
-{
+void MotorControl::displayPinInfo() {
+  Serial.print(F("Motor: "));
+  Serial.print(this->motorIndex);
+  Serial.print(F(" on Pin #"));
+  Serial.print(servos[this->motorIndex].pin);
+  Serial.print(F(" Channel: "));
+  Serial.print(servos[this->motorIndex].channel);
+  Serial.print(F(" Enc Chan A Pin: "));
+  Serial.print(this->enc_a_pin);
+  Serial.print(F(" Enc Chan B Pin: "));
+  Serial.print(this->enc_b_pin);
+  Serial.print(F("\r\n"));
+
+    // Serial.print(F("\r\nDuty Cycle: "));
+    // Serial.print(power2Duty(pwr));
+}
+
+/**
+ * @brief power2Duty convert the [-1, 1] motor value to a timeon value is microseconds, 
+ * then converts to a duty cycle and then scales that to a 16-bit integer, the resolution of the channel
+ * link to math model: https://www.desmos.com/calculator/kmrh5pjrdf
+ * @author Rhys Davies
+ * Updated 2-26-2023
+ * 
+ * input range:  [-1, 1]
+ * output range: [0, 65,536] (Really 26215 to 52429)
+ * 
+ * @param power the input power
+*/
+uint16_t MotorControl::power2Duty(float power) {
+  // this can be written in compiler code, but we are trying to save on flash memory
+  // linearly convert a [-1, 1] motor power to a microseconds value between 1000us and 2000us
+  this->tempTimeon = (power + 1) * 500 + 1000;
+  return (tempTimeon / (PWM_PERIOD * 1000)) * (PWM_MAXDUTY / 1000);
+}
+
+/**
+ * @brief writelow hopefully helps to prevent the issue with motors spinning at 
+ * max speed on boot, sending bots into the stratosphere
+ * 
+ */
+void MotorControl::writelow() {
+    digitalWrite(servos[this->motorIndex].pin, LOW);
+}
+
+/**
+ * @brief NEEDS SPELLCHECK stop derectly writes o to the motor. It also resets the integral adder inside the PI loop. 
+ * This is an esay way to rapidly stop motion on an indeviual motor
+ * 
+ */
+void MotorControl::stop() {
+    this->write(0);
+    if (CL_enable)
+      this->PILoop(0);
+}
+
+/**
+ * @brief NEEDS SPELLCHECK setTargetSpeed is the "main loop" of motor control. It is the profered function to set the speed of a motor
+ * it calls other inportent functions like ramp and PILoop
+ * 
+*/
+void MotorControl::setTargetSpeed(int target_rpm) {
+
+  ramped_speed = this->ramp(target_rpm, 0.00375f); // first call ramp for traction control and to make sure the PI loop dose not use large accerations
+
+  // if there are working encoders its safe to use the PL loop, 
+  // if the encoder fails or is not present the PI loop must be bypased to aviod an out of control robot
+  if (CL_enable) 
+    set_speed = PILoop(ramped_speed);  
+  else 
+    set_speed = ramped_speed;
+
+  this->write(RPM2Percent(set_speed)); //convert speed to the coresponding motor power and write to the motor 
+
+}
+
+/**
+ * @brief NEEDS SPELLCHECK getCurrentSpeed is the interface between the main CPU and the encoder modual 
+ * 
+*/
+int MotorControl::getCurrentSpeed() {
+
+  //! Quantum this is yours to write
+
+  return current_speed; 
+}
+
+/**
+ * @brief NEEDS SPELLCHECK integrate uses trapizodial intgration to calculate the running integral sum for the PI controller
+ * 
+*/
+int MotorControl::integrate(int current_error) {
+
+  integral_sum = integral_sum + 0.5*(current_error + prev_current_error)*(millis()-prev_integral_time);
+  prev_integral_time = millis();
+  prev_current_error = current_error;
+
+  return integral_sum; 
+}
+
+/**
+ * @brief NEEDS SPELLCHECK integrateReset resets the varibles in integral
+ * 
+*/
+void MotorControl::integrateReset() {
+  integral_sum = 0;
+  prev_integral_time = millis();
+}
+
+/**
+ * @brief NEEDS SPELLCHECK PILoop is the closed loop controller. this is the main function for CL  
+ * @author Grant Brautigam
+ * Updated 11-19-2023
+ * 
+*/
+int MotorControl::PILoop(int target_speed) {
+  
+  deadZone = 0.1; // for 10% motor power, the power level at witch the motor no longer turns
+
+  if (abs(target_speed) <= Percent2RPM(deadZone)) { // the motor wants to stop, skip and reset the PI loop  
+    adjusted_speed = 0;
+    integrateReset();
+  } else {
+    error = target_speed - getCurrentSpeed();
+    
+    adjusted_speed = k_p*error + k_i*integrate(error);
+  }
+
+  return adjusted_speed; 
+
+}
+
+/**
+ * @brief 
+ * @author Grant Brautigam
+ * Updated 9-11-2023
+ * 
+ * called on an interrupt
+ * 
+ * when the encoder interrupt is called, read the b pin to see what state it is in
+ * this eliminates the need for two seperate interrupts
+ * 
+*/
+void MotorControl::readEncoder() {
+
+  b_channel_state = digitalRead(this->enc_b_pin);
+
+  if (b_channel_state == 1) {
+    if (encoderACount >= rollover) {
+      encoderACount = 0;
+    } else {
+      encoderACount = encoderACount + 1;
+    }
+      
+  } else {
+    if (encoderACount == 0) {
+      encoderACount = rollover;
+    } else {
+      encoderACount = encoderACount - 1;
+    }  
+  }
+}
+
+/**
+ * @brief 
+ * @author Grant Brautigam
+ * Updated 9-11-2023
+ * 
+ * 
+ * 
+*/
+int MotorControl::calcSpeed(int current_count) {  
+  current_time = millis();
+  
+  //first check if the curret count has rolled over
+  if (abs(current_count - prev_current_count) >= rollover_threshold) {
+    if ((current_count-rollover_threshold)>0) {
+      omega = float ((current_count-rollover)-prev_current_count)/(current_time-prev_current_time);
+    } else {
+      omega = float ((current_count+rollover)-prev_current_count)/(current_time-prev_current_time);
+    }
+  } else {
+    omega = float (current_count-prev_current_count)/(current_time-prev_current_time);
+  }
+
+  prev_current_count = current_count;
+  prev_current_time = current_time;
+
+  return omega*156.25f; // 156.25 for 384, 312.5 for 192, 1250 for 48
+}
+
+int MotorControl::Percent2RPM(float pct) {
   // float temp = constrain(pct, -1, 1);
   return this->max_rpm * constrain(pct, -1.0f, 1.0f);
 }
