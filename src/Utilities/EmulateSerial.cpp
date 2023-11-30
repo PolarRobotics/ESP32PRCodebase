13118ec66c18c2ab544dda75c2e13a50eeae8e1d
Tw1ZZLER <ilekbrownies@gmail.com>
2023-11-29 21:14:26 -0500

    Finished EmulateSerial script

--- src/Utilities/EmulateSerial.cpp
+++ src/Utilities/EmulateSerial.cpp
@@ -1,8 +1,11 @@
+// A very simple script for Data Aq to emulate expected
+// serial output from ESP32, without controller input necessary
+
 #include <PolarRobotics.h>
 
-int main() {
-    while(true) {
-
-    }
-    return 0;
+void setup() {
+    Serial.begin(115200);
+}
+void loop() {
+    Serial.print(F("L_HAT_Y,0.00,R_HAT_X,0.00,Turn,0.46,Left Motor,0.00,Right Motor,0.00\n"));
 }
