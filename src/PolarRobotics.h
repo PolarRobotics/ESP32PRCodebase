dfd90ccd58c41aadca997efbf4a0d1118e5e4251
RyzenFromFire <ryzenfromfire@gmail.com>
2024-01-22 20:58:46 -0500

    fix ver string

--- src/PolarRobotics.h
+++ src/PolarRobotics.h
@@ -13,7 +13,7 @@
 #include <Utilities/MotorTypes.h>
 #include <Utilities/DriveParameters.h>
 
-#define PR_CODEBASE_VERSION "2.3.0 @ production"
+#define PR_CODEBASE_VERSION "2.3.1 @ dev/main"
 
 // [PIN DECLARATIONS]
 // please follow: 
