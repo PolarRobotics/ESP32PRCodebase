dccc88c42a960a3bd9b9aef6f0308da86ec3dce2
RyzenFromFire <ryzenfromfire@gmail.com>
2024-01-31 23:30:41 -0500

    outline quarterback functions

--- src/Robot/QuarterbackTurret.h
+++ src/Robot/QuarterbackTurret.h
@@ -19,6 +19,14 @@ enum CradleState {
   forward, back
 };
 
+enum TargetReceiver {
+  receiver_1, receiver_2
+};
+
+enum FlywheelSpeed {
+  slow_inwards, stopped, slow_outwards, lvl1_outwards, lvl2_outwards, lvl3_outwards, maximum
+};
+
 /**
  * @brief Quarterback Turret Subclass Header
  * @authors Maxwell Phillips
@@ -34,17 +42,36 @@ class QuarterbackTurret : public Robot {
   public:
     QuarterbackTurret();
     void action() override; //! robot subclass must override action
-    void toggleFlywheels();
-    void changeFlywheelSpeed(SpeedStatus speed);
-    void switchMode(TurretMode mode);
-    void switchReceiver();
-    void changeAngle(TurretAngle angle);
-    void moveTurret(int heading);
-    void moveCradle(CradleState state);
-    void receiveBall();
-    void launchBall();
-    void passToCenter();
-    void passToSelectedReceiver();
+
+    //* base/internal functions
+    void moveTurret(int heading); // moves turret/turntable to specific heading. currently relative to robot, not field.
+
+    void setFlywheelSpeed(double absoluteSpeed); // also accessible via manual stick override
+
+    void setFlywheelSpeedStage(FlywheelSpeed stage); // directly set flywheel speed to a stage
+
+    void aimAssembly(TurretAngle angle); // aims the assembly holding the flywheels and cradle (two states).
+
+    void moveCradle(CradleState state); // moves the linear actuator connected to the cradle to one of two states.
+    // used to fire the ball in both manual and automatic modes
+
+    //* derived functions (general)
+    void adjustFlywheelSpeedStage(SpeedStatus speed);
+
+    //* derived functions (automatic targeting)
+    void switchMode(TurretMode mode); // switch between manual and automatic targeting
+    void switchTarget(TargetReceiver target); // switch between targeted receivers. switches to automatic targeting if not already set.
+
+    //* derived functions (manual macros)
+    void loadFromCenter(); // prepares qb to intake ball from center
+    void handoff(); // hands off ball to runningback
+
+    //* setup and safety functions
+    void setEnabled(bool enabled); // toggles turret and flywheel movement
+    void emergencyStop(); 
+    void zeroTurret(); // calibrates turret/moves turret to home/zero (cnc/3d printer style)
+    void reset(); // zero turret, aim down (straight), and set flywheels to slow intake
+    
 };
 
 #endif // QUARTERBACK_TURRET_H
