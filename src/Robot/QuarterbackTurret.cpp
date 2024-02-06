97242f54726273b711185ed725759100d2363e6e
RyzenFromFire <ryzenfromfire@gmail.com>
2024-02-05 20:01:24 -0500

    added fields

--- src/Robot/QuarterbackTurret.cpp
+++ src/Robot/QuarterbackTurret.cpp
@@ -1,5 +1,9 @@
 #include "QuarterbackTurret.h"
 
+void QuarterbackTurret::moveTurret(float power) {
+
+}
+
 
 void QuarterbackTurret::setFlywheelSpeedStage(FlywheelSpeed stage) {
   // flywheelSpeeds[static_cast<uint8_t>(stage)];
--- src/Robot/QuarterbackTurret.h
+++ src/Robot/QuarterbackTurret.h
@@ -11,7 +11,7 @@ enum TurretMode {
   manual, automatic
 };
 
-enum TurretAngle {
+enum AssemblyAngle {
   straight, angled
 };
 
@@ -41,24 +41,57 @@ enum FlywheelSpeed {
  */
 class QuarterbackTurret : public Robot {
   private: 
+    // motor pins
     uint8_t flywheelPin;
     uint8_t turretPin;
+    uint8_t cradlePin;
+
+    // motor instances
     MotorControl flywheelMotor;
     MotorControl turretMotor;
-    bool flywheelsOn;
-    float flywheelSpeedFactor;
+    MotorControl cradleMotor;
+
+    // state
+    bool enabled = false; // default false, set to true upon homing/reset
+
+    // assembly
+    AssemblyAngle currentAssemblyAngle; // initial state unknown, will reset to straight
+    AssemblyAngle targetAssemblyAngle = straight; // default straight
+    bool assemblyMoving = false; // default false
+
+    // mode
+    TurretMode mode = manual; // default manual
+    TargetReceiver target = receiver_1; // default receiver_1
+
+    // flywheel
+    FlywheelSpeed currentFlywheelStage = stopped; // default stopped
+    FlywheelSpeed targetFlywheelStage = stopped;  // default stopped
+
+    float currentFlywheelSpeed = 0; // default 0
+    float targetFlywheelSpeed = 0;  // default 0
+
+    // turret
+    float currentTurretSpeed = 0; // default 0
+    float targetTurretSpeed = 0;  // default 0
+
+    int currentTurretHeading = 0; // default 0
+    int targetTurretHeading = 0;  // default 0
+    
   public:
     QuarterbackTurret();
     void action() override; //! robot subclass must override action
 
     //* base/internal functions
+    void moveTurret(float power); // moves turret at specified power (open loop)
+
     void moveTurret(int heading); // moves turret/turntable to specific heading. currently relative to robot, not field.
+                                  // will not be implemented until MotorControl is stabilized
 
     void setFlywheelSpeed(double absoluteSpeed); // also accessible via manual stick override
 
     void setFlywheelSpeedStage(FlywheelSpeed stage); // directly set flywheel speed to a stage
 
-    void aimAssembly(TurretAngle angle); // aims the assembly holding the flywheels and cradle (two states).
+    void aimAssembly(AssemblyAngle angle); // aims the assembly holding the flywheels and cradle (two states).
 
     void moveCradle(CradleState state); // moves the linear actuator connected to the cradle to one of two states.
     // used to fire the ball in both manual and automatic modes
