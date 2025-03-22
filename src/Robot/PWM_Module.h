3e4670c6425476d8373ffe3ad084a5444f91c173
k-colish <k-colish@onu.edu>
2025-03-21 21:06:32 -0400

    Created PWM_Module
    Created library based on MotorInterface

---
+++ src/Robot/PWM_Module.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#ifndef __PWM_MODULE__
+#define __PWM_MODULE__
+
+#include <Arduino.h>
+#include <Adafruit_PWMServoDriver.h>
+
+#define MAX_NUM_MOTORS 16
+
+// input of 0 is 1500 us
+#define MAX_PWM_US 2000     // Input of 1
+#define MIN_PWM_US 1000     // Input of -1
+
+#define PWM_ADDRESS 0x40    // Default PWM i2c address
+#define PWM_RES 12          // Max PWM Resolution is 12 https://cdn-shop.adafruit.com/datasheets/PCA9685.pdf
+#define PWM_MAXDUTY (1 << PWM_RES) - 1
+// a Period of 2500us for the sabertooth, gives the st enough time to react to inputs, 
+// can make this value closer to 2000us if we have issues with the ST not updating fast enough
+#define PWM_PERIOD 0.0025   // 2500 us
+#define PWM_FREQ 1/PWM_PERIOD
+
+typedef struct servo{
+    uint8_t pin;
+}servo_t;
+
+static servo_t motors[MAX_NUM_MOTORS];
+static uint8_t MotorCount = 0;
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(PWM_ADDRESS);
+
+
+class PWM_Module{
+private:
+    uint8_t motorIndex;     // Index of the Motor
+    int8_t min;             
+    int8_t max;             // maximum PWM value, set based on motor driver (sabertooth is MIN_PWM_US)
+    uint32_t tempTimeon;
+    uint16_t power2Duty(float power);
+
+public:
+    PWM_Module();
+    uint8_t attach(int pin, int min, int max);
+    void write(float power);
+    void displayPinInfo();
+    void writelow();
+};
+
+#endif
