5eebc8424adc697c0272f7cedf1950f969df2f4e
okquentin <qosterhage@outlook.com>
2025-10-04 17:39:46 -0400

    Removed function calls to allow for compilation
    Testing only on lineman for now, will change logic later

--- src/Robot/Center.cpp
+++ src/Robot/Center.cpp
@@ -24,8 +24,8 @@
 Center::Center(uint8_t armPin, uint8_t clawPin) {
   this->armPin = armPin;
   this->clawPin = clawPin;
-  armMotor.setup(armPin);
-  clawMotor.setup(clawPin);
+  // armMotor.setup(armPin);
+  // clawMotor.setup(clawPin);
 }
 
 void Center::action() {
