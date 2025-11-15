18350e8f1943228a3243cc89ed1f33cfa33e52d8
k-colish <k-colish@onu.edu>
2025-11-15 08:37:20 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,7 @@
 // RTOS Includes
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <freertos/queue.h>
 
 // Primary Parent Component Pointers
 Robot* robot = nullptr; // subclassed if needed
@@ -49,17 +50,87 @@ drive_param_t driveParams;
 // Config
 ConfigManager config;
 
+// Encoder variables
+#define ENCODER_EN true
+
+#if ENCODER_EN
+
+#define ENC1_CHA 3
+#define ENC1_CHB 4
+#define ENC2_CHA 5
+#define ENC2_CHB 6
+TaskHandle_t isrTaskHandle = NULL;
+QueueHandle_t queueA = xQueueCreate(10, sizeof(uint32_t));
+QueueHandle_t queueB = xQueueCreate(10, sizeof(uint32_t));
+
+long encoder1_Count = 0;
+long encoder2_Count = 0;
+
+volatile bool ENC2_CHBState;
+volatile bool ENC1_CHBState;
+
+void IRAM_ATTR ENC1_handler(void* args){
+    ENC1_CHBState = digitalRead(ENC1_CHB);
+
+    uint32_t state = ENC1_CHBState ? 1u : 0u;
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    xQueueSendFromISR(queueA, &state, &xHigherPriorityTaskWoken);
+}
+
+void IRAM_ATTR ENC2_handler(void* args){
+    ENC2_CHBState = digitalRead(ENC2_CHB);
+
+    uint32_t state = ENC2_CHBState ? 1u : 0u;
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    xQueueSendFromISR(queueB, &state, &xHigherPriorityTaskWoken);
+}
+
+void xENC1Task( void * pvParameters ){
+    uint32_t ENC_CHB_State;
+
+    for(;;){
+        // Infinite loop to keep the task alive
+        xQueueReceive(queueA, &ENC_CHB_State, portMAX_DELAY);
+        if(ENC_CHB_State == 1){
+            encoder1_Count++;
+        } else {
+            encoder1_Count--;
+        }
+        Serial.printf("Interrupt Triggered, Encoder 1 Count: %d\n" ,encoder1_Count);
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
+void xENC2Task( void * pvParameters ){
+    uint32_t ENC_CHB_State;
+
+    for(;;){
+        // Infinite loop to keep the task alive
+        xQueueReceive(queueB, &ENC_CHB_State, portMAX_DELAY);
+        if(ENC_CHB_State == 1){
+            encoder2_Count--;
+        } else {
+            encoder2_Count++;
+        }
+        Serial.printf("Interrupt Triggered, Encoder 2 Count: %d\n" , encoder2_Count);
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
+
+#endif
+
 // Prototypes for Controller Callbacks
 // Implementations located at the bottom of this file
 void onConnection();
 void onDisconnect();
 
-void loop1();
+void MainLoop();
 
 TaskHandle_t LoopTaskHandle = NULL;
 void LoopTask(void * parameter) {
   for(;;) {
-    loop1();
+    MainLoop();
   }
   vTaskDelay(1);
 }
@@ -79,6 +150,38 @@ void setup() {
 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(TACKLE_PIN, INPUT); // Try INPUT_PULLUP
+  if(ENCODER_EN){
+    gpio_pad_select_gpio(ENC1_CHA);
+    gpio_set_direction((gpio_num_t)ENC1_CHA, GPIO_MODE_INPUT);
+    gpio_pulldown_en((gpio_num_t)ENC1_CHA);
+    gpio_pullup_dis((gpio_num_t)ENC1_CHA);
+    gpio_set_intr_type((gpio_num_t)ENC1_CHA, GPIO_INTR_POSEDGE);
+
+    gpio_pad_select_gpio(ENC1_CHB);
+    gpio_set_direction((gpio_num_t)ENC1_CHB, GPIO_MODE_INPUT);
+    gpio_pulldown_en((gpio_num_t)ENC1_CHB);
+    gpio_pullup_dis((gpio_num_t)ENC1_CHB);
+
+    gpio_pad_select_gpio(ENC2_CHA);
+    gpio_set_direction((gpio_num_t)ENC2_CHA, GPIO_MODE_INPUT);
+    gpio_pulldown_en((gpio_num_t)ENC2_CHA);
+    gpio_pullup_dis((gpio_num_t)ENC2_CHA);
+    gpio_set_intr_type((gpio_num_t)ENC2_CHA, GPIO_INTR_POSEDGE);
+
+    gpio_pad_select_gpio(ENC2_CHB);
+    gpio_set_direction((gpio_num_t)ENC2_CHB, GPIO_MODE_INPUT);
+    gpio_pulldown_en((gpio_num_t)ENC2_CHB);
+    gpio_pullup_dis((gpio_num_t)ENC2_CHB);
+
+    xTaskCreate(xENC1Task, "ENC1Task", 2048, nullptr, 2, &isrTaskHandle);
+    xTaskCreate(xENC2Task, "ENC2Task", 2048, nullptr, 2, &isrTaskHandle);
+    Serial.println("ISR Tasks created.");
+
+    // Install GPIO ISR service and add handlers
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add((gpio_num_t)ENC1_CHA, ENC1_handler, (void *) ENC1_CHA);
+    gpio_isr_handler_add((gpio_num_t)ENC2_CHA, ENC2_handler, (void *) ENC2_CHA);
+  }
 
   // Read robot info from "EEPROM" (ESP32 Preferences) using ConfigManager
   config.read();
@@ -172,7 +275,7 @@ void setup() {
 */
 
 // runs continuously after setup(). controls driving and any special robot functionality during a game
-void loop1() {
+void MainLoop() {
   if (ps5.isConnected()) {
     // Serial.print(F("\r\nConnected"));
     // ps5.setLed(255, 0, 0);   // set LED red
